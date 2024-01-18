ed7bba7c22848468926935e759cef35e33c33910
Henrique Wagner <wagner.ike@gmail.com>
2024-01-17 22:57:05 -0300

    :lipstick: feat: Configuração ESLint e Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './global.css'
+
 import { Button } from './components/ui/button'
 
 export function App() {
- 
   return (
     <>
-     <div className='flex justify-center items-center h-screen'>
-      <Button>Enviar</Button>
-     </div>
+      <div className="flex h-screen items-center justify-center">
+        <Button>Enviar</Button>
+      </div>
     </>
   )
 }
-
-
